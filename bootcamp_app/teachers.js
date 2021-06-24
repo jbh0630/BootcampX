872aeab730ce233a2b8d0be052ba1e7b55327075
jbh0630 <jayjo0630@gmail.com>
2021-06-24 08:45:25 +0000

    Node Postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,24 @@
+const { Client } = require('pg');
+const args = process.argv.slice(2);
+
+const client = new Client({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+client.connect();
+
+client.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${args[0]}'
+ORDER BY teacher;`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  });
+});
