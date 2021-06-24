befadef59dc16cc51b0d1e4f4751117f80ffc85a
jbh0630 <jayjo0630@gmail.com>
2021-06-24 09:07:40 +0000

    SQL Injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,7 @@
 const { Client } = require('pg');
-const args = process.argv.slice(2);
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
 const client = new Client({
   user: 'vagrant',
@@ -9,15 +11,17 @@ const client = new Client({
 });
 client.connect();
 
-client.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${args[0]}%'
-LIMIT ${args[1]};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-});
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+client.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  });
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
-const args = process.argv.slice(2);
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
 
 const client = new Client({
   user: 'vagrant',
@@ -9,15 +10,18 @@ const client = new Client({
 });
 client.connect();
 
-client.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${args[0]}'
-ORDER BY teacher;`)
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`;
+
+client.query(queryString, values)
 .then(res => {
+  console.log('connected');
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   });
